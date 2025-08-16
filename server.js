d0b0c7931782cae50c45552b0d0e1df83ca9ed93
Georges MOUKALA <moukalazamg@gmail.com>
2025-08-16 22:37:30 +0200

    test 3

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const app = express();
 
-//For production
+//For production test
 const corsOptions = {
   origin: 'https://akazi-recruitment-agency.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
